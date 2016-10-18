7022d00e83def7180a423af8de81e179c4632d1e
Arman Rakib <ar365@outlook.com>
2016-10-18 02:36:27 +0100

    ini commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Arman Rakib on 18/10/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
